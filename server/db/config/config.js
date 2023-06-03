9de2522f17faefb6e4d8d69794f4080150ee8efe
anKhav <khavaldzhi.a@gmail.com>
2023-06-03 20:22:58 +0200

    cONFIGURATING PRODUCTION DB

---
+++ server/db/config/config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'postgres',
+    database: "elif_delivery",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "clothes_store_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+};
