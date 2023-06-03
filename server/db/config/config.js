7f99183a1681e712a6071f8b2824206955c30962
anKhav <khavaldzhi.a@gmail.com>
2023-06-03 20:59:17 +0200

    cONFIGURATING PRODUCTION DB

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -15,7 +15,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    url:process.env.DATABASE_URL,
+    username: process.env.USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 };
