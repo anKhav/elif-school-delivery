30f931aaae0da29a1a7ce6282f4fcb487efbb8fd
anKhav <khavaldzhi.a@gmail.com>
2023-06-04 19:23:28 +0200

    console.logs for debugging

--- server/services/orderService.js
+++ server/services/orderService.js
@@ -38,12 +38,7 @@ class OrderService {
 
         const orders = await Promise.all(ordersData.map(async (order) => {
             const productOrder =  await ProductOrderService.getProductOrder(order.dataValues.id)
-            const products = await Promise.all(productOrder.map(async (product) => {
-                console.log(product.dataValues.productId);
-                const res = await ProductService.get(product.dataValues.productId)
-                return res
-            }))
-            return {...order.dataValues,products}
+            return productOrder
             }))
 
         return orders
